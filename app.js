1c60e41532eca30bddefcfbaf3f1a801f3fb4368
Ewelina Dziewiela <ewelina.dziewiela@ravelo.pl>
2017-03-07 15:28:00 +0100

    jquery and styles changes

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
     $(".show-more_btn").click(function() {
-        $(".promotions-container").css("height", "auto");
+        // $(".promotions-container").css("height", "auto");
         $(".show-more_btn").css("display", "none");
         $(".show-less_btn").css("display", "block");
-        // $(".promotions-container").animate({
-        //    overflow: $(".promotions-container").get(0).scrollHeight
-        // }, 400);
-        // return false;
+        $(".promotions-container").animate({
+           height: $(".promotions-container").get(0).scrollHeight
+        }, 400);
+        return false;
     });
     $(".show-less_btn").click(function() {
-        $(".promotions-container").css("height", "650px");
+        // $(".promotions-container").css("height", "650px");
         $(".show-less_btn").css("display", "none");
         $(".show-more_btn").css("display", "block");
+        $('.promotions-container').animate({
+            height: '700px'
+        }, 400);
+        return false;
     })
 });
